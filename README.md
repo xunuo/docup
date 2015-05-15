cb525b39f5995cc46e5cc5a306d48da0aca014a8
xunuo <i@xunuo.com>
2015-05-15 17:14:36 +0800

    update.

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ npm install docup -g
 - bin  
   
 ```
-docup ./sample.js ./output/sample.html --title=docup --type=html --toc=categories --codeUrl=https://github.com/username/project/blob/master/my.js
+docup ./sample.js ./output/sample.html --title="sample.js document" --type=html --toc=categories --codeUrl=https://github.com/username/project/blob/master/my.js
 ```
 
 - api  
--- samples/usage-bin.sh
+++ samples/usage-bin.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ../bin/docup ./sample.js ./output/sample.md 
-../bin/docup ./sample.js ./output/sample.html --title=jsdoc-from-docUp --type=html --toc=categories --codeUrl=https://github.com/username/project/blob/master/my.js
+../bin/docup ./sample.js ./output/sample.html --title="sample.js document" --type=html --toc=categories --codeUrl=https://github.com/username/project/blob/master/my.js
